258f2064b1ebd22ba049b707445c63146d0d85df
Craig <craig@oppkey.com>
2022-06-07 14:29:41 -0700

    added css styling to layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,16 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
     return (
-        <main>
-            <title>About theta360.guide</title>
-            <h1>About theta360.guide</h1>
-            <Link to="/">Back to Home</Link>
-            <p>The THETA X Development Getting Started Guide is created by 
+            <Layout pageTitle="About theta360.guide">
+
+                <p>The THETA X Development Getting Started Guide is created by 
                 theta360.guide, an independent community site run by Oppkey. 
                 The community receives sponsorship from RICOH, but is not an
                 official RICOH site.  Please contact RICOH directly for official
                 information </p>
-
-        </main>
+        </Layout>
     )
 }
 
