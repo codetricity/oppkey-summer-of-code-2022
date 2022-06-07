926e4b0263692c458e2797c11d404fb5cd9f7f1c
Craig <craig@oppkey.com>
2022-06-07 06:27:13 -0700

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About theta360.guide</title>
+            <h1>About theta360.guide</h1>
+            <Link to="/">Back to Home</Link>
+            <p>The THETA X Development Getting Started Guide is created by 
+                theta360.guide, an independent community site run by Oppkey. 
+                The community receives sponsorship from RICOH, but is not an
+                official RICOH site.  Please contact RICOH directly for official
+                information </p>
+
+        </main>
+    )
+}
+
+export default AboutPage
