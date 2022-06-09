c33712b413c301c5aea6951d900d455c3331d007
Craig <craig@oppkey.com>
2022-06-09 11:48:45 -0700

    added image credit

--- src/pages/thetax-starters/{mdx.slug}.js
+++ src/pages/thetax-starters/{mdx.slug}.js
@@ -1,14 +1,21 @@
 import * as React from 'react'
 import {graphql} from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImag, GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 
 
 const ThetaxStarter = ({ data }) => {
+    const image = getImage(data.mdx.frontmatter.hero_image)
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}> 
-        <p>
+        <p>Posted: 
         {data.mdx.frontmatter.date}
+        </p>
+        <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt}/>
+        <p>
+            Photo Credit:{" "}
+            <a href={data.mdx.frontmatter.hero_image_credit_link}> {data.mdx.frontmatter.hero_image_credit_text} </a>
         </p>
         <MDXRenderer>
             {data.mdx.body}
@@ -23,6 +30,14 @@ export const query = graphql`
             frontmatter {
                 title
                 date(formatString: "MMMM D, YYYY")
+                hero_image_alt
+                hero_image_credit_link
+                hero_image_credit_text
+                hero_image {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
             }
             body
         }
