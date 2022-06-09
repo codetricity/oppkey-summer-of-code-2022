849b3a98865df247a319aa353bc7a826f050ebf7
Craig <craig@oppkey.com>
2022-06-09 11:31:02 -0700

    changed struction of images

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `THETA X Development Summer of Code 2022`,
     siteUrl: `https://oppkeysummerofcode2022main.gatsbyjs.io/`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: "gatsby-source-filesystem",
     options: {
       name: `thetax-starters`,
